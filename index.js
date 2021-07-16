91b45f6b93f737ab9fed1dbd9a61ba96496f01cd
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-07-16 00:20:53 -0700

    Add support for yarn and non-standard submodules

--- index.js
+++ index.js
@@ -16,31 +16,70 @@ try {
 }
 
 if (!package.submodules) {
-  console.log("No submodules section defined in package.json, please define an array of glob patterns as the \"submodules\" section.");
-  package.submodules = [];
+  console.log("No submodules section defined in package.json, please define an array of glob patterns or submodule objects as the \"submodules\" section.");
+  return
+}
+
+// Convert any glob strings to submodule objects so as to support non-npm based submodules.
+for (let i = 0; i < package.submodules.length; i++) {
+  if (typeof package.submodules[i] === 'string') {
+    package.submodules[i] = {
+      path: package.submodules[i],
+    }
+  }
+}
+
+// Determine submodule types.
+for (let i = 0; i < package.submodules.length; i++) {
+  if (package.submodules[i].cmd) continue;
+  if (fs.existsSync(path.join(package.submodules[i].path, 'yarn.lock'))) {
+    package.submodules[i].cmd = 'yarn';
+    package.submodules[i].args = package.submodules[i].args || ['install'];
+    package.submodules[i].contains = 'package.json';
+  } else {
+    package.submodules[i].cmd = 'npm';
+    package.submodules[i].args = package.submodules[i].args || ['i'];
+    package.submodules[i].contains = 'package.json';
+  }
+}
+
+// Fix commands for windows.
+if (os.platform().startsWith('win')) {
+  for (let i = 0; i < package.submodules.length; i++) {
+    if (package.submodules[i].cmd === 'yarn') {
+      package.submodules[i].cmd += '.cmd'
+    } else if (package.submodules[i].cmd === 'npm') {
+      package.submodules[i].cmd += '.cmd'
+    }
+  }
 }
 
 // Run glob match for our provided globs
-let submodule_paths = []; // To-be-filled list of submodule paths
-let processed_paths = 0;  // Amount of paths processed for cb resolution
+let submodules_paths = []
+let processed = 0;
 for (let i = 0; i < package.submodules.length; i++) {
-  glob(path.join(package.submodules[i], "package.json"), {}, function (err, files) {
-    // Remove "package.json"
-    files.forEach(function(v, i) {
-      files[i] = path.dirname(v);
-    });
-    // Merge files with main list and deduplicate
-    submodule_paths = submodule_paths.concat(files.filter(function(item) {
-      return submodule_paths.indexOf(item) < 0;
-    }));
-    // If we've processed all the targets, run npm install on each
-    if (++processed_paths == package.submodules.length) {
-      const cmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
-      for (let i = 0; i < submodule_paths.length; i++) {
-        console.log("installing submodule " + submodule_paths[i]);
-        child_process.spawnSync(cmd, ['i'], {
-          env: process.env, cwd: path.resolve(submodule_paths[i]), stdio: 'inherit'
-        });
+  let glob_path = package.submodules[i].path;
+  if (package.submodules[i].contains) {
+    glob_path = path.join(glob_path, package.submodules[i].contains);
+  }
+  glob(glob_path, {}, function (err, files) {
+    // Remove contains.
+    if (package.submodules[i].contains) {
+      files.forEach(function(v, i) {
+        files[i] = path.dirname(v);
+      });
+    }
+    submodules_paths[i] = files;
+    if (++processed === package.submodules.length) {
+      // Install our submodules.
+      for (let i = 0; i < submodules_paths.length; i++) {
+        if (!submodules_paths[i] || submodules_paths[i].length === 0) continue;
+        for (let j = 0; j < submodules_paths[i].length; j++) {
+          console.log("installing submodule " + submodules_paths[i][j]);
+          child_process.spawnSync(package.submodules[i].cmd, package.submodules[i].args, {
+            env: process.env, cwd: path.resolve(submodules_paths[i][j]), stdio: 'inherit'
+          });
+        }
       }
     }
   });
